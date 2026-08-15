feaec599ee44cd5f971d555ad7d574f9227fc427
Prakhar Agarwal <prakharagarwal@192.168.1.10>
2026-08-15 21:13:13 +0800

    Add bidirectionality to relationship type

--- src/domain/relationships/relationship_type.py
+++ src/domain/relationships/relationship_type.py
@@ -9,9 +9,13 @@ class RelationshipType:
     name: str
     source_type: AssetType
     target_type: AssetType
+    is_bidirectional: bool = False
 
     def __post_init__(self) -> None:
         if not (isinstance(self.source_type, AssetType)) or not (
             isinstance(self.target_type, AssetType)
         ):
+            raise InvalidRelationshipTypeException
+
+        if not isinstance(self.is_bidirectional, bool):
             raise InvalidRelationshipTypeException
