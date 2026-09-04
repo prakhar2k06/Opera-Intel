c2a3629d27032c8d392b018ec365c4bcb5747b0b
Prakhar Agarwal <prakhar2k06@gmail.com>
2026-09-04 21:11:38 +0800

    Add property updating functionality and domain event emission

--- src/domain/assets/asset.py
+++ src/domain/assets/asset.py
@@ -1,3 +1,8 @@
+from src.domain.assets.property import Property
+
+from ..events.asset_property_changed_event import AssetPropertyChangedEvent
+from ..events.asset_state_changed_event import AssetStateChangedEvent
+from ..events.domain_event import DomainEvent
 from .asset_type import AssetType
 from .exceptions import (
     InvalidAssetStateTransitionException,
@@ -20,6 +25,7 @@ class Asset:
         self.asset_type: AssetType = asset_type
         self.properties: dict = {}
         self.current_state: State | None = asset_type.initial_state
+        self._domain_events: list[DomainEvent] = []
 
         for property_name in properties:
             if property_name not in self.asset_type.properties:
@@ -61,4 +67,41 @@ class Asset:
         if not self.asset_type.can_transition(self.current_state, target):
             raise InvalidAssetStateTransitionException
 
+        previous_state: State = self.current_state
         self.current_state = target
+
+        self._domain_events.append(
+            AssetStateChangedEvent(
+                asset=self, previous_state=previous_state, new_state=target
+            )
+        )
+
+    def update_property(self, property_name: str, new_value) -> None:
+        if property_name not in self.asset_type.properties:
+            raise UnknownPropertyException
+
+        property_definition: Property = self.asset_type.properties[property_name]
+
+        if not property_definition.property_type.validate(new_value):
+            raise InvalidPropertyValueException
+
+        previous_value = self.properties[property_name]
+
+        if previous_value == new_value:
+            return
+
+        self.properties[property_name] = new_value
+
+        self._domain_events.append(
+            AssetPropertyChangedEvent(
+                asset=self,
+                property=property_definition,
+                previous_value=previous_value,
+                new_value=new_value,
+            )
+        )
+
+    def pull_domain_events(self) -> list:
+        events: list[DomainEvent] = list(self._domain_events)
+        self._domain_events.clear()
+        return events
