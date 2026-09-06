b40049aa842c7c42a39f13fdecf25305dda03d6c
Prakhar Agarwal <prakhar2k06@gmail.com>
2026-09-06 08:18:22 +0800

    Add type check

--- src/domain/rules/trigger.py
+++ src/domain/rules/trigger.py
@@ -6,8 +6,12 @@ from .exceptions import InvalidTriggerDefinitionException
 
 @dataclass(frozen=True)
 class Trigger:
-    trigger_event: DomainEvent
+    trigger_event: type[DomainEvent]
 
     def __post_init__(self) -> None:
-        if not self.trigger_event or not issubclass(self.trigger_event, DomainEvent):
+        if (
+            not self.trigger_event
+            or not isinstance(self.trigger_event, type)
+            or not issubclass(self.trigger_event, DomainEvent)
+        ):
             raise InvalidTriggerDefinitionException
